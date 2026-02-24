94c21e49be40e7d9e0999aa9dce87a184b26cf67
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-02-24 08:15:55 -0500

    Refactor AppleBox class by removing method implementation
    Removed implementation details from the numberOfPies method while retaining the class structure.

--- src/AppleBox.java
+++ src/AppleBox.java
@@ -15,36 +15,11 @@ public class AppleBox {
     public int numberOfPies(double poundsPerPie) 
     {
         /* to be implemented */
-        double weight = 0;
-        for(int row = 0; row < apples.length; row++){
-            for(int col = 0; col < apples[0].length; col++){
-                boolean canUse = true;
-                if(apples[row][col].isRotten()){
-                    canUse = false;
-                }
-                else if(row - 1 >= 0 && apples[row - 1][col].isRotten()){
-                    canUse = false;
-                }
-                else if(row + 1 < apples.length && apples[row + 1][col].isRotten()){
-                    canUse = false;
-                }
-                else if(col - 1 >= 0 && apples[row][col - 1].isRotten()){
-                    canUse = false;
-                }
-                else if(col + 1 < apples[0].length && apples[row][col + 1].isRotten()){
-                    canUse = false;
-                }
-
-                if(canUse){
-                    weight += apples[row][col].getWeight();
-                }               
-            }            
-        }
-        return (int)(weight / poundsPerPie);
     }
 
     /*
      * There may be instance variables, constructors,
      * and methods that are not shown.
      */
+
 }
